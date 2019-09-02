67c569367f5d358daa84a66180df31fa7712e2fa
apartment81 <bogdan.mirodone@gmail.com>
2019-09-02 23:53:22 +0300

    bb

---
+++ src/ro/mirodone/BouncingBalls.java
@@ -0,0 +1,19 @@
+package ro.mirodone;
+
+public class BouncingBalls {
+
+
+    public int bouncingBall(double h, double bounce, double window) {
+
+        if (h <= 0 || bounce <= 0 || bounce >= 1 || window >= h) return -1;
+        double bounceHeight = h * bounce;
+        int count = 1;
+        while (bounceHeight > window) {
+            count += 2;
+            bounceHeight = bounceHeight * bounce;
+        }
+        return count;
+    }
+
+
+}
---
+++ src/ro/mirodone/Main.java
@@ -0,0 +1,13 @@
+package ro.mirodone;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        BouncingBalls bb = new BouncingBalls();
+
+        System.out.println(bb.bouncingBall(3.0, 0.66, 1.5));
+
+    }
+}
